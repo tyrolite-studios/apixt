ec004edc065f6438281b8c6b78c2912cf28334f3
Fatih Gökkurt <fatihuni1912@gmail.com>
2024-07-04 03:41:27 +0200

    handles escapes & quotiation-marks in keys & requested changes

--- web/src/util.js
+++ web/src/util.js
@@ -1,11 +1,11 @@
-const COLOR_CLS = { num: 'red', null: 'blue', bool: 'purple', str: 'maroon',  key: 'teal', default: 'black'}
+const COLOR_CLS = { num: 'crimson', null: 'blue', bool: 'purple', str: 'maroon',  key: 'teal', default: 'black'}
 
 const typeToClass = {
     'boolean': COLOR_CLS.bool,
     'number': COLOR_CLS.num,
     'string': COLOR_CLS.str,
     'object': COLOR_CLS.default,
-};
+}
 
 /**
  * Returns the stringified JSON with Syntax Highlighting
@@ -14,14 +14,18 @@ const typeToClass = {
  */
 const getStringifiedJSON = (myJson, indentation) => {
     if (myJson === undefined) {
-        throw Error("Caution! myJson is undefined");
+        return undefined
     }
     const stringifiedJSON = JSON.stringify(myJson, null, indentation)
     let lines = stringifiedJSON.split('\n')
     lines = lines.map(line => {
+        //These are the indexes where a Quotiation Escape happens
+        const escapeIndexes = [...line.trim().matchAll(/\"/gm)].map(a => a.index)
+
         const end = line.endsWith(',') ? ',' : ''
         line = end === ',' ? line.substring(0, line.length-1) : line
         if (['{', '[', ']', '}'].includes(line.trim())) return line + end //To avoid the opening/closing brackets in further code
+
         const keyVal = line.split(':')
         if (keyVal.length === 1) {
             const type = getTypeOfStringValue(line)
@@ -31,7 +35,26 @@ const getStringifiedJSON = (myJson, indentation) => {
         if (keyVal.length >= 2) {
             let key = ''
             let value = ''
-            const keyIndex = keyVal.findIndex(val => val.endsWith('"'))
+            let keyIndex
+            const possibleEndOfKeys = keyVal.filter(val => val.endsWith('"'))
+            if (possibleEndOfKeys.length > 1) {
+                /*
+                Incase there are multiple Quotiation Marks inside a string before a ":" it handles that problem by checking if the found 
+                Quotiation mark is at one of the escape positions or not. If it is, it gets ignored.
+                */
+                let differenceInLength = 2
+                let position = 0
+                for (const end of possibleEndOfKeys) {
+                    position += end.trim().length
+                    if (!escapeIndexes.includes(position - differenceInLength)) {
+                        keyIndex = keyVal.indexOf(end) 
+                        break
+                    }
+                    differenceInLength--
+                }
+            } else {
+                keyIndex = keyVal.indexOf(possibleEndOfKeys[0])
+            }
             key = '<span class="' + COLOR_CLS.key + '">' + keyVal.slice(0, keyIndex+1).join(':') + '</span>'
             value = keyVal.slice(keyIndex+1).join(':')
             if (!['{', '['].includes(keyVal[1].trim())) {
@@ -52,9 +75,9 @@ const getStringifiedJSON = (myJson, indentation) => {
  */
 const getTypeOfStringValue = (str) => {
     str = str.toLowerCase().trim()
-    if (/^\d+$/.test(str)) {
+    if (/^-?\d+\.?\d*$/.test(str)) { 
         return typeof 1
-    } else if (/true|false/.test(str)) {
+    } else if (/^(true|false)$/.test(str)) {
         return typeof true
     } else if (/null/.test(str)) {
         return typeof null
@@ -62,7 +85,7 @@ const getTypeOfStringValue = (str) => {
     try { 
         const parsed = JSON.parse(str) 
         if (typeof parsed === 'object') {
-            return typeof {};
+            return typeof {}
         } 
     } catch(e) {}
 
--- web/test/util.test.js
+++ web/test/util.test.js
@@ -1,26 +1,26 @@
-import { getStringifiedJSON, getTypeOfStringValue } from "../src/util"
+import { getStringifiedJSON, COLOR_CLS } from "../src/util"
 
 //getStringifiedJSON
-test('should throw error if myJson is undefined', () => {
-    expect(() => getStringifiedJSON(undefined, 2)).toThrow('Caution! myJson is undefined');
+test('should return undefined if myJson is undefined', () => {
+    expect(getStringifiedJSON(undefined, 2)).toBeUndefined();
 });
 test('should handle strings', () => {
-    const expected = `<span class="maroon">"test"</span>`.trim();
+    const expected = `<span class="${COLOR_CLS.str}">"test"</span>`.trim();
 
     expect(getStringifiedJSON("test", 2)).toBe(expected);
 });
 test('should handle numbers', () => {
-    const expected = `<span class="red">3</span>`.trim();
+    const expected = `<span class="${COLOR_CLS.num}">3</span>`.trim();
 
     expect(getStringifiedJSON(3, 2)).toBe(expected);
 });
 test('should handle booleans', () => {
-    const expected = `<span class="purple">true</span>`.trim();
+    const expected = `<span class="${COLOR_CLS.bool}">true</span>`.trim();
 
     expect(getStringifiedJSON(true, 2)).toBe(expected);
 });
 test('should handle null', () => {
-    const expected = `<span class="blue">null</span>`.trim();
+    const expected = `<span class="${COLOR_CLS.null}">null</span>`.trim();
 
     expect(getStringifiedJSON(null, 2)).toBe(expected);
 });
@@ -35,13 +35,13 @@ test('correctly formats JSON with all data types', () => {
     };
     const result = getStringifiedJSON(jsonObject, 2);
 
-    expect(result).toContain('<span class=\"teal\">  \"string\"</span> : <span class=\"maroon\"> \"value\"</span>');
-    expect(result).toContain('<span class=\"teal\">  \"number\"</span> : <span class=\"red\"> 123</span>');
-    expect(result).toContain('<span class=\"teal\">  \"boolean\"</span> : <span class=\"purple\"> true</span>');
-    expect(result).toContain('<span class=\"teal\">  \"nullValue\"</span> : <span class=\"blue\"> null</span>');
-    expect(result).toContain('<span class=\"teal\">  \"object\"</span> :  {');
-    expect(result).toContain('<span class=\"teal\">    \"nested\"</span> : <span class=\"maroon\"> \"object\"</span>');
-    expect(result).toContain('<span class=\"teal\">  \"array\"</span> :  [');
-    expect(result).toContain('<span class=\"maroon\">    \"item1\"</span>');
-    expect(result).toContain('<span class=\"maroon\">    \"item2\"</span>');
+    expect(result).toContain('<span class=\"' + COLOR_CLS.key + '\">  \"string\"</span> : <span class=\"' + COLOR_CLS.str + '\"> \"value\"</span>');
+    expect(result).toContain('<span class=\"' + COLOR_CLS.key + '\">  \"number\"</span> : <span class=\"' + COLOR_CLS.num + '\"> 123</span>');
+    expect(result).toContain('<span class=\"' + COLOR_CLS.key + '\">  \"boolean\"</span> : <span class=\"' + COLOR_CLS.bool + '\"> true</span>');
+    expect(result).toContain('<span class=\"' + COLOR_CLS.key + '\">  \"nullValue\"</span> : <span class=\"' + COLOR_CLS.null + '\"> null</span>');
+    expect(result).toContain('<span class=\"' + COLOR_CLS.key + '\">  \"object\"</span> :  {');
+    expect(result).toContain('<span class=\"' + COLOR_CLS.key + '\">    \"nested\"</span> : <span class=\"' + COLOR_CLS.str + '\"> \"object\"</span>');
+    expect(result).toContain('<span class=\"' + COLOR_CLS.key + '\">  \"array\"</span> :  [');
+    expect(result).toContain('<span class=\"' + COLOR_CLS.str + '\">    \"item1\"</span>');
+    expect(result).toContain('<span class=\"' + COLOR_CLS.str + '\">    \"item2\"</span>');
 });
