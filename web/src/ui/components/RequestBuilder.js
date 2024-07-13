45f89344e16bed909750116f368eb6c25e6c23cb
Fatih Gökkurt <fatihuni1912@gmail.com>
2024-07-13 20:38:16 +0200

    Added possibility to add and remove headers, added recommendations for header and content-types

--- web/src/ui/components/RequestBuilder.js
+++ web/src/ui/components/RequestBuilder.js
@@ -1,13 +1,7 @@
 import { Select, JsonTextarea, Button } from "../commons"
-import { isValidJson, getStringifiedJSON } from "../../util"
 import { useState } from "react"
 import { PathInput } from "./PathInput"
-
-/*
-	TODO
-	JSON Textarea formatierungen
-	Path Logik auf eigene Komponente
-*/
+import { headerContentTypes, requestHeaderOptions } from "../../util"
 
 const RequestBuilder = () => {
     const [method, setMethod] = useState("")
@@ -25,6 +19,7 @@ const RequestBuilder = () => {
     const [jsonIsValid, setJsonIsValid] = useState(true)
     const [headersVisible, setHeadersVisible] = useState(false)
     const [editingHeader, setEditingHeader] = useState(null)
+    const [newHeader, setNewHeader] = useState("")
 
     const handleMethodChange = (selectedMethod) => {
         setMethod(selectedMethod)
@@ -49,10 +44,27 @@ const RequestBuilder = () => {
     }
 
     const handleHeaderValueChange = (key, newValue) => {
+        if (!newValue) newValue = "<Enter Value>"
         setHeaders({
             ...headers,
             [key]: newValue
         })
+    }
+
+    const handleDeleteHeader = (key) => {
+        const updatedHeaders = { ...headers }
+        delete updatedHeaders[key]
+        setHeaders(updatedHeaders)
+    }
+
+    const addNewHeader = () => {
+        if (newHeader.trim() !== "" && !headers.hasOwnProperty(newHeader)) {
+            setHeaders((prevHeaders) => ({
+                ...prevHeaders,
+                [newHeader]: requestHeaderOptions[newHeader] || "<Enter Value>"
+            }))
+            setNewHeader("")
+        }
     }
 
     const handleSubmit = () => {
@@ -129,29 +141,147 @@ const RequestBuilder = () => {
                                 className="text-white flex justify-between items-center"
                             >
                                 <span>{key}</span>
-                                {editingHeader === key ? (
-                                    <input
-                                        className="ml-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                        type="text"
-                                        value={value}
-                                        onChange={(e) =>
-                                            handleHeaderValueChange(
-                                                key,
-                                                e.target.value
-                                            )
-                                        }
-                                        onBlur={() => setEditingHeader(null)}
-                                    />
-                                ) : (
-                                    <span
-                                        onClick={() => setEditingHeader(key)}
-                                        className="cursor-pointer"
-                                    >
-                                        {value}
-                                    </span>
-                                )}
+                                <div className="flex flex-col">
+                                    {editingHeader === key ? (
+                                        <input
+                                            className="ml-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                            type={
+                                                isNaN(value) ? "text" : "number"
+                                            }
+                                            value={
+                                                value === "<Enter Value>"
+                                                    ? ""
+                                                    : value
+                                            }
+                                            onChange={(e) => {
+                                                handleHeaderValueChange(
+                                                    key,
+                                                    e.target.value
+                                                )
+                                            }}
+                                            onKeyDown={(e) => {
+                                                if (e.key === "Enter") {
+                                                    setEditingHeader(null)
+                                                }
+                                            }}
+                                            onBlur={(e) => {
+                                                if (
+                                                    !e.relatedTarget ||
+                                                    !e.relatedTarget.closest(
+                                                        "div"
+                                                    )
+                                                ) {
+                                                    setEditingHeader(null)
+                                                    handleHeaderValueChange(
+                                                        key,
+                                                        value
+                                                    )
+                                                }
+                                            }}
+                                            autoFocus
+                                        />
+                                    ) : (
+                                        <span
+                                            onClick={() =>
+                                                setEditingHeader(key)
+                                            }
+                                            className="cursor-pointer flex items-center"
+                                        >
+                                            {value === "<Enter Value>" ? (
+                                                <div className="text-red-600">
+                                                    {value}
+                                                </div>
+                                            ) : (
+                                                value
+                                            )}
+                                            <button
+                                                className="text-sm text-red-500 p-1 ml-2"
+                                                onClick={() =>
+                                                    handleDeleteHeader(key)
+                                                }
+                                            >
+                                                X
+                                            </button>
+                                        </span>
+                                    )}
+                                    {editingHeader === key &&
+                                    key === "Content-Type" ? (
+                                        <ul className="absolute bg-gray-700 border border-gray-500 rounded mt-8 z-10 max-h-48 overflow-auto">
+                                            {headerContentTypes.map(
+                                                (type) =>
+                                                    type
+                                                        .toLowerCase()
+                                                        .includes(
+                                                            value.toLowerCase()
+                                                        ) && (
+                                                        <li
+                                                            className="p-1 cursor-pointer hover:bg-gray-600 text-left"
+                                                            onMouseDown={(e) =>
+                                                                e.preventDefault()
+                                                            }
+                                                            onClick={() => {
+                                                                handleHeaderValueChange(
+                                                                    key,
+                                                                    type
+                                                                )
+                                                                setEditingHeader(
+                                                                    null
+                                                                )
+                                                            }}
+                                                        >
+                                                            {type}
+                                                        </li>
+                                                    )
+                                            )}
+                                        </ul>
+                                    ) : null}
+                                </div>
                             </div>
                         ))}
+                        <div className="mt-2">
+                            <input
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                type="text"
+                                placeholder="New Header Property"
+                                value={newHeader}
+                                onChange={(e) => setNewHeader(e.target.value)}
+                                onBlur={addNewHeader}
+                            />
+                            {newHeader.trim() !== "" && (
+                                <div className="absolute bg-gray-700 border border-gray-500 rounded z-10 max-h-48 overflow-auto">
+                                    {Object.keys(requestHeaderOptions)
+                                        .filter((header) =>
+                                            header
+                                                .toLowerCase()
+                                                .includes(
+                                                    newHeader.toLowerCase()
+                                                )
+                                        )
+                                        .map((header) => (
+                                            <div
+                                                key={header}
+                                                className="p-1 text-white cursor-pointer hover:bg-gray-600"
+                                                onClick={() => {
+                                                    setHeaders(
+                                                        (prevHeaders) => ({
+                                                            ...prevHeaders,
+                                                            [header]:
+                                                                "<Enter Value>"
+                                                        })
+                                                    )
+                                                    setEditingHeader(null)
+                                                    setNewHeader("")
+                                                }}
+                                                onMouseDown={(e) =>
+                                                    e.preventDefault()
+                                                }
+                                            >
+                                                {header}
+                                            </div>
+                                        ))}
+                                </div>
+                            )}
+                        </div>
                     </div>
                 ) : (
                     <div className="text-white bg-gray-700 p-2 rounded text-sm">
