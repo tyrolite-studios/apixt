3b1ae9449b0e2f0b8fac59443f7a175c7789dd9c
Marc Stein <marc.stein@gfk.com>
2024-07-19 23:43:00 +0200

    Use tokenizer for highlighting and rewrite tests

--- web/src/util.js
+++ web/src/util.js
@@ -1,99 +1,62 @@
-const COLOR_CLS = { num: 'crimson', null: 'blue', bool: 'purple', str: 'maroon',  key: 'teal', default: 'black'}
+import { TokenizeJsonString } from "./tokenizer"
 
-const typeToClass = {
-    'boolean': COLOR_CLS.bool,
-    'number': COLOR_CLS.num,
-    'string': COLOR_CLS.str,
-    'object': COLOR_CLS.default,
+const COLOR_CLS = {
+    num: "text-crimson",
+    null: "text-blue",
+    bool: "text-purple",
+    str: "text-maroon",
+    key: "text-teal",
+    default: "text-black"
+}
+
+const tokenTypeToCls = {
+    bool: COLOR_CLS.bool,
+    number: COLOR_CLS.num,
+    string: COLOR_CLS.str,
+    key: COLOR_CLS.key,
+    null: COLOR_CLS.null,
+    "symbol-[": COLOR_CLS.default,
+    "symbol-]": COLOR_CLS.default,
+    "symbol-{": COLOR_CLS.default,
+    "symbol-}": COLOR_CLS.default,
+    "symbol-:": COLOR_CLS.default,
+    "symbol-,": COLOR_CLS.default
 }
 
 /**
- * Returns the stringified JSON with Syntax Highlighting
- * @param {object | Array} myJson The Object to stringify.
- * @returns {string} The stringified Object.
+ * Returns the stringified JSON with syntax highlighting
+ *
+ * @param {mixed} input The Object to stringify
+ * @param {number} indentation The number of spaces for indentation
+ *
+ * @returns {string} The stringified JSON of the input
  */
-const getStringifiedJSON = (myJson, indentation) => {
-    if (myJson === undefined) {
-        return undefined
-    }
-    const stringifiedJSON = JSON.stringify(myJson, null, indentation)
-    let lines = stringifiedJSON.split('\n')
-    lines = lines.map(line => {
-        //These are the indexes where a Quotiation Escape happens
-        const escapeIndexes = [...line.trim().matchAll(/\\"/gm)].map(a => a.index)
+const getStringifiedJSON = (input, indentation) => {
+    const jsonStr = JSON.stringify(input, null, indentation)
+    if (!jsonStr) return undefined
 
-        const end = line.endsWith(',') ? ',' : ''
-        line = end === ',' ? line.substring(0, line.length-1) : line
-        if (['{', '[', ']', '}'].includes(line.trim())) return line + end //To avoid the opening/closing brackets in further code
+    const tokens = TokenizeJsonString(jsonStr)
 
-        const keyVal = line.split(':')
-        if (keyVal.length === 1) {
-            const type = getTypeOfStringValue(line)
-            const cls = (type === 'object' && line.trim() === 'null') ? COLOR_CLS.null : typeToClass[type]
-            return '<span class="' + cls +'">' + line + '</span>' + end
-        }
-        if (keyVal.length >= 2) {
-            let key = ''
-            let value = ''
-            let keyIndex
-            const possibleEndOfKeys = keyVal.filter(val => val.endsWith('"'))
-            if (possibleEndOfKeys.length > 1) {
-                /*
-                Incase there are multiple Quotiation Marks inside a string before a ":" it handles that problem by checking if the found 
-                Quotiation mark is at one of the escape positions or not. If it is, it gets ignored.
-                */
-                let differenceInLength = 2
-                let position = 0
-                for (const end of possibleEndOfKeys) {
-                    position += end.trim().length
-                    if (!escapeIndexes.includes(position - differenceInLength)) {
-                        keyIndex = keyVal.indexOf(end) 
-                        break
-                    }
-                    differenceInLength--
-                }
-            } else {
-                keyIndex = keyVal.indexOf(possibleEndOfKeys[0])
+    let result = ""
+    let pos = 0
+    const lastPos = tokens.length - 1
+    while (pos <= lastPos) {
+        let { type, raw, end } = tokens[pos]
+        if (type === "string" && pos < lastPos && tokens[pos + 1].raw === ":") {
+            // object key
+            type = "key"
+        } else if (end && pos < lastPos) {
+            // empty object or array
+            if (tokens[pos + 1].raw === end) {
+                raw += end
+                pos++
             }
-            key = '<span class="' + COLOR_CLS.key + '">' + keyVal.slice(0, keyIndex+1).join(':') + ':</span>'
-            value = keyVal.slice(keyIndex+1).join(':')
-            if (!['{', '['].includes(keyVal[1].trim())) {
-                const type = getTypeOfStringValue(value)
-                const cls = (type === 'object' && value.trim() === 'null') ? COLOR_CLS.null : typeToClass[type]
-                value = ' <span class="' + cls +'">' + value  + '</span>' + end
-            }
-            return key + value
         }
-    })
-    return lines.join('\n')
-}
-
-/**
- * Returns the type of the value of the string
- * @param {string} str The string to evaluate.
- * @returns {string} The type of the evaluated string.
- */
-const getTypeOfStringValue = (str) => {
-    str = str.toLowerCase().trim()
-    if (/^-?\d+\.?\d*$/.test(str)) { 
-        return typeof 1
-    } else if (/^(true|false)$/.test(str)) {
-        return typeof true
-    } else if (/null/.test(str)) {
-        return typeof null
+        const color = tokenTypeToCls[type] ? tokenTypeToCls[type] : null
+        result += color ? `<span class="${color}">${raw}</span>` : raw
+        pos++
     }
-    try { 
-        const parsed = JSON.parse(str) 
-        if (typeof parsed === 'object') {
-            return typeof {}
-        } 
-    } catch(e) {}
+    return result
+}
 
-    return typeof ""
-}
- 
-export {
-    getStringifiedJSON,
-    getTypeOfStringValue,
-    COLOR_CLS
-}
+export { getStringifiedJSON, COLOR_CLS }
--- web/test/util.test.js
+++ web/test/util.test.js
@@ -1,126 +1,69 @@
 import { getStringifiedJSON, COLOR_CLS } from "../src/util"
-import { TokenizeJsonString } from "../src/tokenizer"
 
-// syntax highlighting mit tokenizer...
-const highlight = (input, indentation) => {
-    const jsonStr = JSON.stringify(input, null, indentation)
-    if (!jsonStr) return undefined
-
-    const tokens = TokenizeJsonString(jsonStr)
-
-    const type2cls = {
-        bool: COLOR_CLS.bool,
-        number: COLOR_CLS.num,
-        string: COLOR_CLS.str,
-        key: COLOR_CLS.key,
-        null: COLOR_CLS.null,
-        'symbol-[': COLOR_CLS.default,
-        'symbol-]': COLOR_CLS.default,
-        'symbol-{': COLOR_CLS.default,
-        'symbol-}': COLOR_CLS.default,
-        // normalerweise sollten wir hier auch "symbol-," und "symbol-:" einfärben
-    }
-    
-    let result = ''
-    let pos = 0
-    const lastPos = tokens.length - 1
-    while (pos <= lastPos) {
-        let { type, raw, end } = tokens[pos]
-        if (type === 'string' && pos < lastPos && tokens[pos + 1].raw === ':') {
-            // object key
-            type = 'key'
-            raw += ':'
-            pos++
-        } else if (end && pos < lastPos) {
-            // empty object or array
-            if (tokens[pos + 1].raw === end) {
-                raw += end
-                pos++
-            }
-        }
-        const color = type2cls[type] ? type2cls[type] : null
-        result += color ? `<span class="${color}">${raw}</span>` : raw
-        pos++
-    }
-    return result
-}
-
-const compareHighlighting = (cases) => {
-    test(`Compare highlighting results`, () => {
-        for (const json of cases) {
-            const actual = getStringifiedJSON(json, 2)
-            const expected = highlight(json, 2)
-            expect(actual, 'Original: ' + JSON.stringify(json)).toEqual(expected)
-        }
-    })
-}
-
-compareHighlighting(
-    [
-        undefined,
-        null,
-        0,
-        -1,
-        0.5,
-//        -5.6E+24,                      // number mit Exponenten werden nicht erkannt...
-//        0.5E-8,                       
+const colorToValues = {
+    [COLOR_CLS.null]: [null],
+    [COLOR_CLS.num]: [0, -1, 0.5, -5.6e24, 0.5e-8],
+    [COLOR_CLS.str]: [
         "",
         "abc",
         "\\",
-        "\\\"",
-//        "\\\":",                      // das dürfte bekannt sein
+        '\\"',
+        '\\":',
         "\n",
         "\r\n",
         "\t",
         "{\n}",
         "[]",
-        "\uffff",
+        "\uffff"
+    ],
+    [COLOR_CLS.default]: [
         [],
-//        [[]],                         // hier wird der äußere Array-start und das Ende nicht eingefärbt
-//        [{}],
-//        [1, 2],                       // im Prinzip nicht falsch, aber der span beinhaltet hier auch die whitespaces
+        [[]],
+        [{}],
+        [1, 2],
         {},
-//        {foo: 'bar'},                 // hier wird jeweils der Object-start und das Ende nicht eingefärbt
-//        {foo: 'bar', foo2: 'bar2'},   
-//        {foo: {}},
-/*                 
-        {
-            string: "value",
-            number: 123,
-            boolean: true,
-            nullValue: null,
-            object: { nested: "object", deep: [0, 10] },
-            array: ["item1", "item2"]
-        }
-            */
+        { foo: "bar" },
+        { foo: "bar", foo2: "bar2" },
+        { foo: {} }
     ]
-)
+}
+
+test("first token color should match", () => {
+    for (const [color, values] of Object.entries(colorToValues)) {
+        const expected = `<span class="${color}">`
+        for (const value of values) {
+            expect(
+                getStringifiedJSON(value, 2).substring(0, expected.length)
+            ).toBe(expected)
+        }
+    }
+})
 
 //getStringifiedJSON
-test('should return undefined if myJson is undefined', () => {
-    expect(getStringifiedJSON(undefined, 2)).toBeUndefined();
-});
-test('should handle strings', () => {
-    const expected = `<span class="${COLOR_CLS.str}">"test"</span>`.trim();
+test("should return undefined if myJson is undefined", () => {
+    expect(getStringifiedJSON(undefined, 2)).toBeUndefined()
+})
+test("should handle strings", () => {
+    const expected = `<span class="${COLOR_CLS.str}">"test"</span>`.trim()
 
-    expect(getStringifiedJSON("test", 2)).toBe(expected);
-});
-test('should handle numbers', () => {
-    const expected = `<span class="${COLOR_CLS.num}">3</span>`.trim();
+    expect(getStringifiedJSON("test", 2)).toBe(expected)
+})
+test("should handle numbers", () => {
+    const expected = `<span class="${COLOR_CLS.num}">3</span>`.trim()
 
-    expect(getStringifiedJSON(3, 2)).toBe(expected);
-});
-test('should handle booleans', () => {
-    const expected = `<span class="${COLOR_CLS.bool}">true</span>`.trim();
+    expect(getStringifiedJSON(3, 2)).toBe(expected)
+})
+test("should handle booleans", () => {
+    const expected = `<span class="${COLOR_CLS.bool}">true</span>`.trim()
 
-    expect(getStringifiedJSON(true, 2)).toBe(expected);
-});
-test('should handle null', () => {
-    const expected = `<span class="${COLOR_CLS.null}">null</span>`.trim();
+    expect(getStringifiedJSON(true, 2)).toBe(expected)
+})
+test("should handle null", () => {
+    const expected = `<span class="${COLOR_CLS.null}">null</span>`.trim()
 
-    expect(getStringifiedJSON(null, 2)).toBe(expected);
-});
-test('correctly formats JSON with all data types', () => {
+    expect(getStringifiedJSON(null, 2)).toBe(expected)
+})
+test("correctly formats JSON with all data types", () => {
     const jsonObject = {
         string: "value",
         number: 123,
@@ -128,21 +71,41 @@ test('correctly formats JSON with all data types', () => {
         nullValue: null,
         object: { nested: "object" },
         array: ["item1", "item2"]
-    };
-    const result = getStringifiedJSON(jsonObject, 2);
+    }
+    const result = getStringifiedJSON(jsonObject, 2)
 
-    expect(result).toContain(`<span class="${COLOR_CLS.key}">  "string":</span> <span class="${COLOR_CLS.str}"> "value"</span>`);
-    expect(result).toContain(`<span class="${COLOR_CLS.key}">  "number":</span> <span class="${COLOR_CLS.num}"> 123</span>`);
-    expect(result).toContain(`<span class="${COLOR_CLS.key}">  "boolean":</span> <span class="${COLOR_CLS.bool}"> true</span>`);
-    expect(result).toContain(`<span class="${COLOR_CLS.key}">  "nullValue":</span> <span class="${COLOR_CLS.null}"> null</span>`);
-    expect(result).toContain(`<span class="${COLOR_CLS.key}">  "object":</span> {`);
-    expect(result).toContain(`<span class="${COLOR_CLS.key}">    "nested":</span> <span class="${COLOR_CLS.str}"> "object"</span>`);
-    expect(result).toContain(`<span class="${COLOR_CLS.key}">  "array":</span> [`);
-    expect(result).toContain(`<span class="${COLOR_CLS.str}">    "item1"</span>`);
-    expect(result).toContain(`<span class="${COLOR_CLS.str}">    "item2"</span>`);
-});
-test('correctly handle multiple quotiation marks in key', () => {
-    const jsonObject = {"\":6\":\":\"test\":\":6\"::::::\"\"\"\"\"6": "failure"}
-    const result = getStringifiedJSON(jsonObject, 2);
-    expect(result).toContain(`<span class="${COLOR_CLS.key}">  \"\\\":6\\\":\\\":\\\"test\\\":\\\":6\\\"::::::\\\"\\\"\\\"\\\"\\\"6\":</span> <span class="${COLOR_CLS.str}"> \"failure\"</span>`)
+    expect(result).toContain(
+        `  <span class="${COLOR_CLS.key}">"string"</span><span class="${COLOR_CLS.default}">:</span> <span class="${COLOR_CLS.str}">"value"</span>`
+    )
+    expect(result).toContain(
+        `  <span class="${COLOR_CLS.key}">"number"</span><span class="${COLOR_CLS.default}">:</span> <span class="${COLOR_CLS.num}">123</span>`
+    )
+    expect(result).toContain(
+        `  <span class="${COLOR_CLS.key}">"boolean"</span><span class="${COLOR_CLS.default}">:</span> <span class="${COLOR_CLS.bool}">true</span>`
+    )
+    expect(result).toContain(
+        `  <span class="${COLOR_CLS.key}">"nullValue"</span><span class="${COLOR_CLS.default}">:</span> <span class="${COLOR_CLS.null}">null</span>`
+    )
+    expect(result).toContain(
+        `  <span class="${COLOR_CLS.key}">"object"</span><span class="${COLOR_CLS.default}">:</span> <span class="${COLOR_CLS.default}">{</span>`
+    )
+    expect(result).toContain(
+        `    <span class="${COLOR_CLS.key}">"nested"</span><span class="${COLOR_CLS.default}">:</span> <span class="${COLOR_CLS.str}">"object"</span>`
+    )
+    expect(result).toContain(
+        `  <span class="${COLOR_CLS.key}">"array"</span><span class="${COLOR_CLS.default}">:</span> <span class="${COLOR_CLS.default}">[</span>`
+    )
+    expect(result).toContain(
+        `    <span class="${COLOR_CLS.str}">"item1"</span>`
+    )
+    expect(result).toContain(
+        `    <span class="${COLOR_CLS.str}">"item2"</span>`
+    )
+})
+test("correctly handle multiple quotiation marks in key", () => {
+    const jsonObject = { '":6":":"test":":6"::::::"""""6': "failure" }
+    const result = getStringifiedJSON(jsonObject, 2)
+    expect(result).toContain(
+        `<span class="${COLOR_CLS.key}">\"\\\":6\\\":\\\":\\\"test\\\":\\\":6\\\"::::::\\\"\\\"\\\"\\\"\\\"6\"</span><span class="${COLOR_CLS.default}">:</span> <span class="${COLOR_CLS.str}">\"failure\"</span>`
+    )
 })
