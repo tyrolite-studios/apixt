12020bca2317dc644d5d0b84e5aabac72de0bcbe
Marc Stein <marc.stein@gfk.com>
2024-08-26 08:51:01 +0200

    Remove go related code and make web folder the new root

--- apixt_test.go
+++
@@ -1,9 +0,0 @@
-package apixt
-
-import "testing"
-
-func TestInit(t *testing.T) {
-	if false {
-		t.Error("Unexpected result!")
-	}
-}
--- go.mod
+++
@@ -1,5 +0,0 @@
-module apixt
-
-go 1.22.1
-
-require github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
